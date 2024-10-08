e5a161d6ca9854976812ca99213e48bd56ed82b4
nathankaique <119627814+nathankaique@users.noreply.github.com>
2024-10-08 16:55:08 -0300

    Add files via upload

---
+++ exercicio3-atividade2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Defina os valores diretamente no código
+        double valor = 68.00;      // Valor a ser investido
+        double taxa1 = 2.00/100;         // Taxa de juros (%)// Converte para decimal (taxa de 5.30% = 0.053)
+        int meses = 3;              // Tempo de investimento em meses
+
+        // Exibir valores iniciais
+        Console.WriteLine($"Valor investido: R$ {valor}");
+        Console.WriteLine($"Taxa de juros: {taxa1*100}%");
+        Console.WriteLine($"Tempo de investimento: {meses} meses");
+
+        // Cálculo do valor futuro com juros compostos
+        double valorFuturo = valor * Math.Pow(1 + taxa1, meses);
+
+        // Exibe o valor futuro arredondado com 2 casas decimais
+        Console.WriteLine($"Valor Futuro: R$ {Math.Round(valorFuturo, 2)}");
+    }
+}
