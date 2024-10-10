4a399fa10399075ec7245e2969d2394801060adf
nathankaique <119627814+nathankaique@users.noreply.github.com>
2024-10-10 14:10:30 -0300

    Add files via upload

--- exercicio4-atividade2/Program.cs
+++ exercicio4-atividade2/Program.cs
@@ -2,13 +2,18 @@
 
 class Program {
     public static void Main(string[] args) {
-        // Definindo valores diretamente
+        // Definindo valores diretamente pelo usuário
         double[] rendimentos = new double[12]; // Array para armazenar rendimentos mensais
 
-        // Definindo valores iniciais
-        double valorPresente = 2000.00; // Exemplo: R$ 1000,00
-        double taxaJuros = 2.00; // Exemplo: 5% ao mês
-        int periodo = 6; // Exemplo: 3 meses
+        // Entrada de valores iniciais pelo usuário
+        Console.Write("Digite o valor presente: ");
+        double valorPresente = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a taxa de juros ao mês: ");
+        double taxaJuros = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o período: ");
+        int periodo = Convert.ToInt32(Console.ReadLine());
 
         // Cálculo do valor futuro
         double valorFuturo = valorPresente * (Math.Pow((taxaJuros / 100) + 1, periodo));
@@ -21,9 +26,9 @@ class Program {
         double rendaAcumulada = valorFuturo + rendaLiquida;
         Console.WriteLine($"Renda acumulada após {periodo} meses: R$ {Math.Round(rendaAcumulada, 2)}");
 
-        // Definindo valor do saque
-        double saque = 200.00; // Exemplo: R$ 200,00
-        Console.WriteLine($"saque de: R$ {Math.Round(saque, 2)}");
+        // Entrada do valor de saque
+        Console.Write("Digite o valor do saque: ");
+        double saque = Convert.ToDouble(Console.ReadLine());
 
         // Cálculo do saldo após o saque
         double saldo = rendaAcumulada - saque;
