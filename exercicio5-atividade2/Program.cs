888fb4157d0c4e4c80f4bf038fc33f9472e85079
nathankaique <119627814+nathankaique@users.noreply.github.com>
2024-10-10 14:15:35 -0300

    Add files via upload

--- exercicio5-atividade2/Program.cs
+++ exercicio5-atividade2/Program.cs
@@ -4,16 +4,21 @@ class Program
 {
     public static void Main(string[] args)
     {
-        // Definindo os valores
-        double valorFuturo = 7390.61; // Valor Futuro em R$
-        double taxaJuros = 0.05; // Taxa de juros de 5% ao ano (0,05)
-        int numeroPeriodos = 2; // Número de períodos (anos)
+        // Entrada de valores pelo usuário
+        Console.Write("Digite o valor futuro : ");
+        double valorFuturo = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite a taxa de juros ao ano: ");
+        double taxaJuros = Convert.ToDouble(Console.ReadLine()) / 100;
+
+        Console.Write("Digite o número de períodos: ");
+        int numeroPeriodos = Convert.ToInt32(Console.ReadLine());
 
         // Cálculo do Valor Presente (VP)
         double valorPresente = valorFuturo / Math.Pow(1 + taxaJuros, numeroPeriodos);
 
         // Exibindo os resultados
-        Console.WriteLine($"Valor Futuro: R$ {valorFuturo}");
+        Console.WriteLine($"\nValor Futuro: R$ {valorFuturo}");
         Console.WriteLine($"Taxa de Juros: {taxaJuros * 100}%");
         Console.WriteLine($"Número de Períodos: {numeroPeriodos} anos");
         Console.WriteLine($"Valor Presente: R$ {Math.Round(valorPresente, 2)}");
